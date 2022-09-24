bd512595d6b4b3667904a87b5097668435f60d02
Oh_My_Goat <daftouney@gmail.com>
2022-09-24 17:01:32 +0200

    docs: ajout documentation itérateur album :memo:

--- src/features/artists/components/albums/AlbumIterator.tsx
+++ src/features/artists/components/albums/AlbumIterator.tsx
@@ -2,9 +2,19 @@ import * as React from "react";
 import { ArrayField, Button } from "react-admin";
 import { AlbumsList, AlbumCreate } from "./";
 
+/**
+ * Composant qui affiche la liste des albums de l'artiste et permet de les modifier
+ * @returns Composant d'affichage/modification des albums d'un artiste
+ */
 export default function AlbumIterator() {
+    
     const [toggle, setToggle] = React.useState(false);
+
+    /**
+     * Change le boolean toggle pour afficher le formulaire d'ajout d'album
+     */
     const handleClick = () => setToggle(!toggle);
+
   return (
     <>
       <ArrayField source="albums">
