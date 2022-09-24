8cce26ba999f66a09a3a630a3fe70b48b1ca043c
Oh_My_Goat <daftouney@gmail.com>
2022-09-24 16:51:04 +0200

    feat: ajout boutton de suppression

--- src/features/artists/components/albums/AlbumsList.tsx
+++ src/features/artists/components/albums/AlbumsList.tsx
@@ -1,11 +1,50 @@
 import * as React from "react";
-import { Datagrid, DateField, TextField } from "react-admin";
+import {
+  Button,
+  Datagrid,
+  DateField,
+  FunctionField,
+  TextField,
+  useEditController,
+  useRecordContext,
+} from "react-admin";
 
 export default function AlbumsList() {
+  const record = useRecordContext();
+  const { save } = useEditController({ redirect: "show" });
+
+  /**
+   * Récupère l'index de l'élément dans la liste
+   */
+  const getAlbumIndex = (title: any) => {
+    return record.albums.findIndex((album: any) => album.title === title);
+  };
+
+  /**
+   * Permet de supprimer l'album
+   */
+  const deleteAlbum = (albumIndex: string) => {
+    record.albums.splice(albumIndex, 1);
+  };
+
+  const handleClick = (title: any) => {
+    const albumIndex = getAlbumIndex(title);
+    deleteAlbum(albumIndex);
+    console.log(record)
+    save?.(record);
+  };
+
   return (
     <Datagrid bulkActionButtons={false}>
       <TextField source="title" />
       <DateField source="release_date" />
+      <FunctionField
+        render={(record: any) => {
+          return (
+            <Button onClick={() => handleClick(record.title)} label="Supprimer" />
+          );
+        }}
+      />
     </Datagrid>
   );
 }
