c519032d1d3d35bad8329d0fa94c89fe3b813134
Oh_My_Goat <daftouney@gmail.com>
2022-09-24 16:57:31 +0200

    docs: ajout documentation AlbumList :memo:

--- src/features/artists/components/albums/AlbumsList.tsx
+++ src/features/artists/components/albums/AlbumsList.tsx
@@ -9,28 +9,44 @@ import {
   useRecordContext,
 } from "react-admin";
 
+/**
+ * Affiche les albums de l'artiste sous forme de liste.
+ * Doit être enfant d'une ArrayField.
+ * @returns Liste des albums
+ */
 export default function AlbumsList() {
   const record = useRecordContext();
   const { save } = useEditController({ redirect: "show" });
 
   /**
-   * Récupère l'index de l'élément dans la liste
+   * Récupère l'index de l'album à partir de son titre et de sa date de sortie.
+   * @param title Titre de l'album
+   * @returns L'index de l'album dans la liste
    */
-  const getAlbumIndex = (title: any) => {
-    return record.albums.findIndex((album: any) => album.title === title);
+  const getAlbumIndex = (title: string, releaseDate: Date) => {
+    return record.albums.findIndex(
+      (album: any) =>
+        album.title === title && album.release_date === releaseDate
+    );
   };
 
   /**
-   * Permet de supprimer l'album
+   * Supprime un album de la liste des albums de l'artiste à partir de son index.
+   * @param albumIndex Index de l'album à supprimer
    */
   const deleteAlbum = (albumIndex: string) => {
     record.albums.splice(albumIndex, 1);
   };
 
-  const handleClick = (title: any) => {
-    const albumIndex = getAlbumIndex(title);
+  /**
+   * Supprime un album à partir de son titre et de sa date de sortie.
+   * @param title titre de l'album
+   * @param releaseDate date de sortie de l'albul
+   */
+  const handleClick = (title: any, releaseDate: Date) => {
+    const albumIndex = getAlbumIndex(title, releaseDate);
     deleteAlbum(albumIndex);
-    console.log(record)
+    console.log(record);
     save?.(record);
   };
 
@@ -41,7 +57,10 @@ export default function AlbumsList() {
       <FunctionField
         render={(record: any) => {
           return (
-            <Button onClick={() => handleClick(record.title)} label="Supprimer" />
+            <Button
+              onClick={() => handleClick(record.title, record.release_date)}
+              label="Supprimer"
+            />
           );
         }}
       />
